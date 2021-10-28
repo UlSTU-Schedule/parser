79473f7ad9bc3e3f593a7fcdd01d1a7de3b87e78
Pavel Kurmyza <tmrrwnxtsn@gmail.com>
2021-10-28 22:17:52 +0400

    Complement SubLesson's String meth

--- types/types.go
+++ types/types.go
@@ -88,9 +88,12 @@ type SubLesson struct {
 
 // String returns a string representation of SubLesson.
 func (sl SubLesson) String() string {
-	if sl.Teacher != "" {
-		return fmt.Sprintf("%s %s, %s, аудитория %s", sl.Type, sl.Name, sl.Teacher, sl.Room)
-	} else {
-		return fmt.Sprintf("%s %s, аудитория %s", sl.Type, sl.Name, sl.Room)
+	if sl.Name != "" {
+		if sl.Teacher != "" {
+			return fmt.Sprintf("%s %s, %s, аудитория %s", sl.Type, sl.Name, sl.Teacher, sl.Room)
+		} else {
+			return fmt.Sprintf("%s %s, аудитория %s", sl.Type, sl.Name, sl.Room)
+		}
 	}
+	return ""
 }
